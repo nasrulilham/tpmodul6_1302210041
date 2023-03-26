36c180c735cf379f99385bb4f9dd0f1a0e03487e
nasrulilham <91175018+nasrulilham@users.noreply.github.com>
2023-03-26 21:07:47 +0700

    “menambahkan class SayaTubeVideo

--- tpmodul6_1302210041/tpmodul6_1302210041/Program.cs
+++ tpmodul6_1302210041/tpmodul6_1302210041/Program.cs
@@ -1,2 +1,39 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+
+using System.Diagnostics.Contracts;
+
+public class SayaTubeVideo
+{
+    int id;
+    string title;
+    int playCount;
+
+    public SayaTubeVideo(string t)
+    {
+        this.title = t;
+        Random x = new Random();
+        id = x.Next(10000,99999);
+        this.playCount = 0;
+    }
+
+    public void IncreasePlayCount(int pc)
+    {
+        this.playCount += pc;
+    }
+
+    public void PrintVideoDetails()
+    {
+        Console.WriteLine("id       : " + this.id);
+        Console.WriteLine("title    : " + this.title);
+        Console.WriteLine("playCount: " + this.playCount);
+    }
+
+    public static void Main(string[] args)
+    {
+        SayaTubeVideo video = new SayaTubeVideo("Tutorial Design By Contract – Nasrul");
+        video.IncreasePlayCount(11);
+        video.PrintVideoDetails();
+    }
+
+}
+
